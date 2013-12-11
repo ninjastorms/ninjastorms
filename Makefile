bdde7abad03fdd85d604638d405ef29ee08b617d
Andreas Grapentin <andreas@grapentin.org>
2013-12-11 20:04:12 +0100

    beautified u-boot-stubs.c, made convoluted makefile lines silent

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 
+
+
 PREFIX = arm-none-linux-gnueabi-
 CC = $(PREFIX)gcc
 AR = $(PREFIX)ar
@@ -11,7 +13,7 @@ SDMNT = /mnt/sd1
 LOADADDR = 0xC1000000
 ENTRYADDR = 0xC1000004
 
-CFLAGS = -g -Os -fno-common -ffixed-r8 -msoft-float -fno-common -ffixed-r8 -msoft-float -D__KERNEL__ -DTEXT_BASE=0xC1080000 -I./include -fno-builtin -ffreestanding -nostdinc -isystem /usr/local/bin/../lib/gcc/arm-none-linux-gnueabi/4.3.3/include -pipe -DCONFIG_ARM -D__ARM__ -marm -mabi=aapcs-linux -mno-thumb-interwork -march=armv5te -march=armv5te -Wall -Wstrict-prototypes -fno-stack-protector
+CFLAGS = -g -Os -fno-common -ffixed-r8 -msoft-float -fno-common -ffixed-r8 -msoft-float -D__KERNEL__ -DTEXT_BASE=0xC1080000 -I./include -fno-builtin -ffreestanding -nostdinc -isystem /usr/local/bin/../lib/gcc/arm-none-linux-gnueabi/4.3.3/include -pipe -DCONFIG_ARM -D__ARM__ -marm -mabi=aapcs-linux -mno-thumb-interwork -march=armv5te -march=armv5te -Wall -Wstrict-prototypes -fno-stack-protector -Wall -Wextra -Werror
 
 LDFLAGS = -g -Ttext $(LOADADDR) -L/usr/local/bin/../lib/gcc/arm-none-linux-gnueabi/4.3.3 -lgcc 
 
@@ -26,34 +28,40 @@ SREC = $(ELF).srec
 all: $(ELF) $(SREC) $(BIN)
 
 $(ELF): $(OBJ)
-	$(LD) $(LDFLAGS) -o $(ELF) -e $(ELF)_main $(OBJ) 
+	@echo "  LD  $@"
+	@$(LD) $(LDFLAGS) -o $(ELF) -e $(ELF)_main $(OBJ) 
 
 $(BIN): $(ELF)
-	$(OBJCOPY) -O binary $(ELF) $(BIN)
+	@echo "  OBJCOPY  $(BIN)"
+	@$(OBJCOPY) -O binary $(ELF) $(BIN)
 
 $(SREC): $(ELF)
-	$(OBJCOPY) -O srec $(ELF) $(SREC)
+	@echo "  OBJCOPY  $(SREC)"
+	@$(OBJCOPY) -O srec $(ELF) $(SREC)
 
 boot.scr: boot.cmd
-	mkimage -C none -A arm -T script -d boot.cmd boot.scr
+	@echo "  MKIMAGE  boot.scr"
+	@mkimage -C none -A arm -T script -d boot.cmd boot.scr &> /dev/null
 
 boot.cmd:
-	echo "fatload mmc 0 $(LOADADDR) $(BIN)" > boot.cmd
-	echo "go $(ENTRYADDR)" >> boot.cmd
+	@echo "fatload mmc 0 $(LOADADDR) $(BIN)" > boot.cmd
+	@echo "go $(ENTRYADDR)" >> boot.cmd
 
 deploy: $(BIN) boot.scr
-	mkdir -p $(SDMNT)
-	mount $(SDDEV) $(SDMNT)
-	rm -rf $(SDMNT)/*
-	cp $(BIN) $(SDMNT)/
-	cp boot.scr $(SDMNT)/
-	umount $(SDDEV)
+	@echo "  INSTALL  $(BIN) -> $(SDDEV)"
+	@mkdir -p $(SDMNT)
+	@mount $(SDDEV) $(SDMNT)
+	@cp $(BIN) $(SDMNT)/
+	@cp boot.scr $(SDMNT)/
+	@umount $(SDDEV)
 
 clean:
-	rm -f $(OBJ) $(ELF) $(BIN) $(SREC) boot.scr boot.cmd
+	@rm -f src/*.o src/*.a $(ELF) $(BIN) $(SREC) boot.scr boot.cmd
 
 %.o: %.c
-	$(CC) $(CFLAGS) -o $@ $< -c
+	@echo "  CC  $<"
+	@$(CC) $(CFLAGS) -o $@ $< -c
 
 lib%.a: %.o
-	$(AR) crv $@ $<
+	@echo "  AR  $@"
+	@$(AR) crv $@ $<
--- src/u-boot-stubs.c
+++ src/u-boot-stubs.c
@@ -5,63 +5,51 @@
 
 /* enumerate all u-boot functions we use
  */
-enum {
-  XF_get_version,
-  XF_printf,
-	XF_MAX
-};
+enum 
+  {
+    XF_get_version,
+    XF_printf,
+	  XF_MAX
+  };
 
 #define CONFIG_NR_DRAM_BANKS	1 /* we have 1 bank of DRAM */
 
 /* uboot data structure
  */
-typedef struct bd_info {
-    int                 bi_baudrate;    /* serial console baudrate */
-    unsigned long       bi_ip_addr;     /* IP Address */
-    struct environment_s               *bi_env;
-    unsigned long               bi_arch_number; /* unique id for this board */
-    unsigned long               bi_boot_params; /* where this board expects params */
-    struct                              /* RAM configuration */
-    {
+struct bd_info 
+  {
+    int                     bi_baudrate;    /* serial console baudrate */
+    unsigned long           bi_ip_addr;     /* IP Address */
+    struct environment_s*   bi_env;
+    unsigned long           bi_arch_number; /* unique id for this board */
+    unsigned long           bi_boot_params; /* where this board expects params */
+    struct                                  /* RAM configuration */
+      {
         unsigned long start;
         unsigned long size;
-    }                   bi_dram[CONFIG_NR_DRAM_BANKS];
-} bd_t;
+      }                     bi_dram[CONFIG_NR_DRAM_BANKS];
+  };
+  
+typedef struct bd_info bd_t;
 
 /* global data structure
  */
-typedef	struct	global_data {
-	bd_t		*bd;
-	unsigned long	flags;
-	unsigned long	baudrate;
-	unsigned long	have_console;	/* serial_init() was called */
-	unsigned long	env_addr;	/* Address  of Environment struct */
-	unsigned long	env_valid;	/* Checksum of Environment valid? */
-	unsigned long	fb_base;	/* base address of frame buffer */
-#ifdef CONFIG_VFD
-	unsigned char	vfd_type;	/* display type */
-#endif
-#if 0
-	unsigned long	cpu_clk;	/* CPU clock in Hz!		*/
-	unsigned long	bus_clk;
-	phys_size_t	ram_size;	/* RAM size */
-	unsigned long	reset_status;	/* reset status register at boot */
-#endif
-	void		**jt;		/* jump table */
-} gd_t;
+struct global_data 
+  {
+	  bd_t		*bd;
+	  unsigned long	flags;
+	  unsigned long	baudrate;
+	  unsigned long	have_console;	/* serial_init() was called */
+	  unsigned long	env_addr;	/* Address  of Environment struct */
+	  unsigned long	env_valid;	/* Checksum of Environment valid? */
+	  unsigned long	fb_base;	/* base address of frame buffer */
+    //#ifdef CONFIG_VFD
+	  //  unsigned char	vfd_type;	/* display type */
+    //#endif
+	  void		**jt;		/* jump table */
+  };
 
-/*
- * Global Data Flags
- */
-#define	GD_FLG_RELOC	0x00001		/* Code was relocated to RAM		*/
-#define	GD_FLG_DEVINIT	0x00002		/* Devices have been initialized	*/
-#define	GD_FLG_SILENT	0x00004		/* Silent mode				*/
-#define	GD_FLG_POSTFAIL	0x00008		/* Critical POST test failed		*/
-#define	GD_FLG_POSTSTOP	0x00010		/* POST seqeunce aborted		*/
-#define	GD_FLG_LOGINIT	0x00020		/* Log Buffer has been initialized	*/
-#define GD_FLG_DISABLE_CONSOLE	0x00040		/* Disable console (in & out)	 */
-
-#define DECLARE_GLOBAL_DATA_PTR     register volatile gd_t *gd asm ("r8")
+typedef struct global_data gd_t;
 
 /* r8 holds the pointer to the global_data, ip is a call-clobbered
  * register
@@ -72,7 +60,7 @@ typedef	struct	global_data {
 #x ":\n"				\
 "	ldr	ip, [r8, %0]\n"		\
 "	ldr	pc, [ip, %1]\n"		\
-	: : "i"(offsetof(gd_t, jt)), "i"(XF_ ## x * sizeof(void *)) : "ip");
+	: : "i"(offsetof(gd_t, jt)), "i"(XF_ ## x * sizeof(void *)) : "ip")
 
 /* This function is necessary to prevent the compiler from
  * generating prologue/epilogue, preparing stack frame etc.
@@ -84,8 +72,8 @@ typedef	struct	global_data {
 void __attribute__((unused)) 
 dummy (void)
 {
-  EXPORT_FUNC(get_version)
-  EXPORT_FUNC(printf)
+  EXPORT_FUNC(get_version);
+  EXPORT_FUNC(printf);
 }
 
 #undef EXPORT_FUNC
