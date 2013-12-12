a359ac50e2e450ebc4285ced4b6eee6d8a18895a
Andreas Grapentin <andreas@grapentin.org>
2013-12-12 21:32:54 +0100

    added simplest conversion modifiers to vprintf

--- include/stdio.h
+++ include/stdio.h
@@ -11,11 +11,18 @@
 # define EOF (-1)
 #endif
 
+#ifndef __may_fail
+# define __may_fail __attribute__((warn_unused_result))
+#endif
+
+#ifndef __check_format
+# define __check_format __attribute__((format (printf, 1, 2)))
+#endif
 
 int putchar(int c);
 
 int puts(const char * __restrict s);
 
-int printf(const char * __restrict format, ...);
+int printf(const char * __restrict format, ...) __check_format;
 
 int vprintf(const char * __restrict format, va_list ap);
--- libc/stdio/vprintf.c
+++ libc/stdio/vprintf.c
@@ -13,8 +13,29 @@ vprintf (const char *format, __attribute__((unused)) va_list ap)
 
   while (*c)
     {
-      int res = putchar(*c);
-      if (__builtin_expect(res == EOF, 0))
+      if (*c == '%')
+        {
+          ++c;
+          switch (*c)
+            {
+            case '%':
+              if (__builtin_expect(putchar('%') == EOF, 0))
+                return EOF;
+              break;
+            case 'c':
+              {
+                int x = va_arg(ap, int);
+                if (__builtin_expect(putchar(x) == EOF, 0))
+                  return EOF;
+              }
+              break;
+            default:
+              printf("\nprintf: unimplemented conversion character '%c'\n", *c);
+              return -1;
+              break;
+            }
+        }
+      if (__builtin_expect(putchar(*c) == EOF, 0))
         return EOF;
       ++chars_written;
       ++c;
