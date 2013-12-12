f4bbdfff37d8977538cc0d5620ba840636d87de7
Andreas Grapentin <andreas@grapentin.org>
2013-12-12 21:37:38 +0100

    fixed whitespace in startup.c

--- src/startup.c
+++ src/startup.c
@@ -4,9 +4,9 @@ extern unsigned long __bss_start, _end;
 void
 startup (void)
 {
-	/* Zero out BSS */
-	unsigned char * cp = (unsigned char *) &__bss_start;
-	while (cp < (unsigned char *)&_end)
-		*cp++ = 0;
+  /* Zero out BSS */
+  unsigned char * cp = (unsigned char *) &__bss_start;
+  while (cp < (unsigned char *)&_end)
+    *cp++ = 0;
 }
 
