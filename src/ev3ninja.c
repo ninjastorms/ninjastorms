e2ada204c96be7357622eccfc0800f0d0c2e1b8a
Christian Werling <crwerling@gmail.com>
2015-12-14 16:01:53 -0500

    some more interrupt testing

--- src/ev3ninja.c
+++ src/ev3ninja.c
@@ -16,17 +16,52 @@
 #define D (-110)
 
 // Versatile stuff
-#define TIMER1_LOAD (volatile unsigned int*)(0x101E2000)
-#define TIMER1_VALUE (volatile unsigned int*)(0x101E2004)
-#define TIMER1_CTRL (volatile char*)(0x101E2008)
+#define TIMER1_BASE 0x101E2000
+#define TIMER1_INTBIT 1 << 4
+
+#define TIMER1_LOAD (volatile unsigned int*)(TIMER1_BASE+0x0)
+#define TIMER1_VALUE (volatile unsigned int*)(TIMER1_BASE+0x4)
+#define TIMER1_CTRL (volatile char*)(TIMER1_BASE+0x8)
+#define TIMER1_INTCLR (volatile char*)(TIMER1_BASE+0xC)
+#define TIMER1_RIS (volatile char*)(TIMER1_BASE+0x10)
+#define TIMER1_MIS (volatile char*)(TIMER1_BASE+0x14)
+
+// Primary Interrupt Controller (PL190)
+#define PIC_BASE 0x10140000
+#define PIC_INTENABLE (volatile char*)(PIC_BASE+0x10)
+
+#define NOP_SLIDE 0x1003c0
 
 int 
 ev3ninja_main (void)
 {
-  // Let's try to set a timer
+  // Overwrite Interrupt Service Routines
+  *(volatile unsigned int*)(0x10) = NOP_SLIDE;
+  *(volatile unsigned int*)(0x14) = NOP_SLIDE;
+  *(volatile unsigned int*)(0x18) = NOP_SLIDE;
+  *(volatile unsigned int*)(0x1000034) = NOP_SLIDE;
+  *(volatile unsigned int*)(PIC_BASE+0x34) = NOP_SLIDE;
+  // Unmask Timer1's Interrupts
+  *PIC_INTENABLE |= TIMER1_INTBIT;
+  
+  // Software Interrupt
+  //asm(
+  //  "SWI 1\n\t"
+  //  );
+  
+
+  // Let's set a timer
   *TIMER1_CTRL &= ~(1 << 7); // unset TimerEn(abled)
+  
+  // Enable IRQs
+  asm(
+    "MRS   r1,cpsr\n\t"
+    "BIC   r1,r1, #0x80\n\t"
+    "MSR   cpsr_c, r1\n\t"
+  );
 
   *TIMER1_CTRL |= 1 << 6;    // set periodic-mode
+  *TIMER1_INTCLR = (char)0x1; // clear interrupts
   *TIMER1_CTRL |= 1 << 5;    // set IntEnable
   *TIMER1_CTRL |= 1 << 1;    // set 32-bit mode
   *TIMER1_CTRL |= 1 << 0;    // set OneShot-Mode
@@ -34,9 +69,30 @@ ev3ninja_main (void)
   *TIMER1_LOAD = 0x5000;     // load
   *TIMER1_CTRL |= 1 << 7;    // set TimerEn(abled)
 
-  while(*TIMER1_VALUE)
+  // Print the countdown
+  while(*TIMER1_VALUE) {
       printf("%x\n", *TIMER1_VALUE);
+      printf("Interrupt? %x", *TIMER1_RIS);
+  }
+
+  // A little nop slide (weeeeeehhh!!!!)
+  asm(
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    "MOV    r0,r0\n\t"
+    );
   
+  *TIMER1_INTCLR = (char)0x1; // clear interrupts
+
   puts("This is EV3 NinjaStorms");
   puts("  shuriken ready");
   
