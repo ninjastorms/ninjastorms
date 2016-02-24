68456d8ded94a050f47dee099068354fc7786a3d
Andreas Grapentin <andreas@grapentin.org>
2016-02-24 11:31:51 +0100

    migrated the build to autotools (still untested)

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+set -u
+
+mkdir -p m4
+autoreconf --install
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-sudo chmod 666 /dev/ttyUSB0
-make boot.scr 1> /dev/null
-echo "C-a C-s"
-echo "choose file: ev3ninja.bin"
-cat boot.cmd
-
-picocom -b 115200 --send-cmd="sb -vv" /dev/ttyUSB0
