3eb0243438d6578ae103d454d54fed3ee7b570e5
Andreas Grapentin <andreas@grapentin.org>
2016-02-26 10:25:04 +0100

    added copyright headers to libc code

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -1,4 +1,23 @@
 
+/******************************************************************************
+ *       ninjastorms - shuriken operating system                              *
+ *                                                                            *
+ *    Copyright (C) 2013 - 2016  Andreas Grapentin et al.                     *
+ *                                                                            *
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+
 #pragma once
 
 /*
@@ -8,21 +27,21 @@
 #include <stdarg.h>
 
 #ifndef EOF
-# define EOF (-1)
+#  define EOF (-1)
 #endif
 
 #ifndef __may_fail
-# define __may_fail __attribute__((warn_unused_result))
+#  define __may_fail __attribute__((warn_unused_result))
 #endif
 
 #ifndef __check_format
-# define __check_format __attribute__((format (printf, 1, 2)))
+#  define __check_format __attribute__((format (printf, 1, 2)))
 #endif
 
 int putchar(int c);
 
-int puts(const char * __restrict s);
+int puts (const char * __restrict s);
 
-int printf(const char * __restrict format, ...) __check_format;
+int printf (const char * __restrict format, ...) __check_format;
 
-int vprintf(const char * __restrict format, va_list ap);
+int vprintf (const char * __restrict format, va_list ap);
--- libc/include/string.h
+++ libc/include/string.h
@@ -1,4 +1,23 @@
 
+/******************************************************************************
+ *       ninjastorms - shuriken operating system                              *
+ *                                                                            *
+ *    Copyright (C) 2013 - 2016  Andreas Grapentin et al.                     *
+ *                                                                            *
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+
 #pragma once
 
 /*
@@ -7,8 +26,8 @@
 
 #include <stddef.h>
 
-void *memset(void * __restrict s, int c, size_t n);
+void *memset (void * __restrict s, int c, size_t n);
 
-void *memcpy(void * __restrict dest, const void * __restrict src, size_t n);
+void *memcpy (void * __restrict dest, const void * __restrict src, size_t n);
 
-int memcmp(const void *s1, const void *s2, size_t n);
+int memcmp (const void *s1, const void *s2, size_t n);
