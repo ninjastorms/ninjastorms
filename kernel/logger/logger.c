b2a7c93da01e6fcace204127eef5efba6ab5e4d5
Lorenz Woth <lorenz.woth@outlook.com>
2020-06-24 17:47:08 +0200

    fix indentation

--- kernel/logger/logger.c
+++ kernel/logger/logger.c
@@ -5,17 +5,17 @@
 void 
 general_log(uint32_t severity, const char* file, const char* format, ...)
 {
-    va_list args;
+  va_list args;
 
-    // Align shorter severities on the same level
-    if(severity == 1 || severity == 2) 
-      printf("%s[%s] \x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
-    else 
-      printf("%s[%s]\x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
+  // Align shorter severities on the same level
+  if(severity == 1 || severity == 2) 
+    printf("%s[%s] \x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
+  else 
+    printf("%s[%s]\x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
     
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
+  va_start(args, format);
+  vprintf(format, args);
+  va_end(args);
 
-    putchar('\n');
+  putchar('\n');
 }
