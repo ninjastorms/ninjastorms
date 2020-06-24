0c3ff18b927de984d7cf5ff2c40e623ec10ba9b2
Lorenz Woth <lorenz.woth@outlook.com>
2020-06-24 17:45:51 +0200

    Fix some indentation and bracket stuff

--- kernel/logger/logger.c
+++ kernel/logger/logger.c
@@ -9,14 +9,10 @@ general_log(uint32_t severity, const char* file, const char* format, ...)
 
     // Align shorter severities on the same level
     if(severity == 1 || severity == 2) 
-      {
-        printf("%s[%s] \x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
-      } 
+      printf("%s[%s] \x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
     else 
-      {
-        printf("%s[%s]\x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
-      }
-
+      printf("%s[%s]\x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
+    
     va_start(args, format);
     vprintf(format, args);
     va_end(args);
--- kernel/logger/logger.h
+++ kernel/logger/logger.h
@@ -12,18 +12,18 @@ void general_log(uint32_t severity, const char* file, const char* format, ...);
 
 static const char* log_severities[] =
 {
-    "DEBUG",
-    "INFO",
-    "WARN",
-    "ERROR",
-    "FATAL"
+  "DEBUG",
+  "INFO",
+  "WARN",
+  "ERROR",
+  "FATAL"
 };
 
 static const char* log_colors[] =
 {
-    "\x1b[37m", // white
-    "\x1b[34m", // blue
-    "\x1b[33m", // yellow
-    "\x1b[91m", // light red
-    "\x1b[31m", // red
+  "\x1b[37m", // white
+  "\x1b[34m", // blue
+  "\x1b[33m", // yellow
+  "\x1b[91m", // light red
+  "\x1b[31m", // red
 };
