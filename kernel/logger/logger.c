07cde821b298bb66180ab65f8e67f78454ac25da
Lorenz Woth <lorenz.woth@outlook.com>
2020-06-24 17:29:30 +0200

    Fix indentations and add new lines

--- kernel/logger/logger.c
+++ kernel/logger/logger.c
@@ -2,20 +2,24 @@
 #include <stdarg.h>
 #include "logger.h"
 
-void general_log(uint32_t severity, const char* file, const char* format, ...)
-  {
+void 
+general_log(uint32_t severity, const char* file, const char* format, ...)
+{
     va_list args;
 
     // Align shorter severities on the same level
-    if(severity == 1 || severity == 2) {
-      printf("%s[%s] \x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
-    } else {
-      printf("%s[%s]\x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
-    }
+    if(severity == 1 || severity == 2) 
+      {
+        printf("%s[%s] \x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
+      } 
+    else 
+      {
+        printf("%s[%s]\x1b[0m [%s]: ", log_colors[severity], log_severities[severity], file);
+      }
 
     va_start(args, format);
     vprintf(format, args);
     va_end(args);
 
     putchar('\n');
-  }
+}
--- kernel/logger/logger.h
+++ kernel/logger/logger.h
@@ -11,19 +11,19 @@
 void general_log(uint32_t severity, const char* file, const char* format, ...);
 
 static const char* log_severities[] =
-  {
+{
     "DEBUG",
     "INFO",
     "WARN",
     "ERROR",
     "FATAL"
-  };
+};
 
 static const char* log_colors[] =
-  {
+{
     "\x1b[37m", // white
     "\x1b[34m", // blue
     "\x1b[33m", // yellow
     "\x1b[91m", // light red
     "\x1b[31m", // red
-  };
+};
